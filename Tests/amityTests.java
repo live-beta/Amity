08fa83c461ff39b111fca893a65ca5d8c33ec6aa
live-beta <wanjala.sam.s@gmail.com>
2017-08-15 22:52:39 +0300

    load(base_line):initial commit - the init load

---
+++ Tests/amityTests.java
@@ -0,0 +1,48 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import junit.framework.*;
+/**
+*
+* Unit tests for core amity functionality
+*
+*/
+public class amityTests {
+	/* 
+		Create fixtures here.
+	*/
+
+	@Test
+	public void tests_create_new_room(){
+
+	}
+
+	@Test
+	public void test_add_person(){
+
+	}
+
+	@Test
+	public void test_reallocate_person(){
+		/* Assert the before condition*/
+		/* Assert the After condition*/
+	}
+
+	@Test
+	public void test_load_people_from_database(){
+
+	}
+
+	@Test
+	public void test_print_people_allocated(){
+
+	}
+
+	@Test
+	public void test_save_state(){
+
+	}
+	@Test
+	public void load_state(){
+		
+	}
+}
