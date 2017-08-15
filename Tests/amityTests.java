7103c33516aebc6921e97bf7462984ef2f5fa542
live-beta <wanjala.sam.s@gmail.com>
2017-08-15 23:47:09 +0300

    add(unit_tests):tests for add person and create-room
    - tests that a working space and/or a living areas are created successfully
    - ensures that the correct category is specified
    [feature #1]

--- Tests/amityTests.java
+++ Tests/amityTests.java
@@ -1,30 +1,58 @@
+
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import junit.framework.*;
+
+
 /**
 *
 * Unit tests for core amity functionality
 *
 */
 public class amityTests {
-	/* 
-		Create fixtures here.
-	*/
 
-	@Test
-	public void tests_create_new_room(){
+	 String [] person_data;
+	 String [] room_data;
+	 String [] office_data;
+
+
+	public void setUp(){
+
+		/* Initialize objects and data */
+		Person person_obj = new  Person();
+		Room room_obj = new Room();
+		person_data = new String[]{"SAMMY WANJALA","FELLOW", "Y"};
+	    room_data = new String [] {"Room 1","Room 2"};
+		office_data = new String [] {"Area 1","Area 2","Area 3"};
 
 	}
 
 	@Test
-	public void test_add_person(){
+	public void test_that_a_working_space_has_been_created_successfully(){
+		
+		/*Tests a new office can be added successfully */
+
+		assertEquals(room_obj(office_data,"OFFICE"), "OFFICE space Added Successfully");
+
+	}
+	@Test
+
+	public void test_that_a_new_living_area_has_been_created_successfully(){
+		
+		assertEquals(room_obj(room_data,"LIVING"),"LIVING area has been creted successfully");
+	
+	}
+
+	@Test
+	public void test_that_a_new_fellow_has_been_added_successfully(){
+
+		assertEquals(person_obj(person_data),"FELLOW has been added successfully");
 
 	}
 
 	@Test
 	public void test_reallocate_person(){
-		/* Assert the before condition*/
-		/* Assert the After condition*/
+
 	}
 
 	@Test
@@ -43,6 +71,6 @@ public class amityTests {
 	}
 	@Test
 	public void load_state(){
-		
+
 	}
 }
