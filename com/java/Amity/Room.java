3755a67ae4915d577118a9411dcf7d97eddf8474
live-beta <wanjala.sam.s@gmail.com>
2017-08-17 05:34:28 +0300

    add(add_room): skeleton function add new room - place holder for adding new room [Builds #Task-01]

---
+++ com/java/Amity/Room.java
@@ -0,0 +1,20 @@
+/**
+* Class that manages the data and functionality
+* of rooms
+*/
+public class Room {
+
+	/*Constructor class*/
+	public void room(String room_names){
+
+		String [] room_name= new String []{room_names};
+
+	}
+	public string add_room(){
+		String message
+
+		return message;
+
+	}
+
+}
