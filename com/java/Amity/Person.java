8de889148fa357916e1978860385d2855c2f1ac4
live-beta <wanjala.sam.s@gmail.com>
2017-08-17 05:27:37 +0300

    add(add_person): add skeleton class - add skeleton class in People functionality [builds #Task-01]

---
+++ com/java/Amity/Person.java
@@ -0,0 +1,22 @@
+package Person;
+
+/**
+* Class that contains functionality for handling people
+*/
+public class Person extends Room {
+
+ /* Constructor function*/
+
+ public void Person(){
+
+ 	String name, category,wants_accomodation;
+ 	String room_name;
+ 	String officeSpaceName;
+ }
+ public string add_person(){
+ 	String message;
+
+ 	return message;
+ }
+ 
+}
