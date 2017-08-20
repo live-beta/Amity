cf12f0fec9822c30d1c18d42412d8efab94a1c8e
live-beta <wanjala.sam.s@gmail.com>
2017-08-20 07:44:55 +0300

    test(person_test): refactor person test strucure
    - add person tests to different suit
    [Builds #Task-01]

---
+++ Tests/personTests.java
@@ -0,0 +1,34 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import junit.framework.*;
+
+/**
+*
+* Unit test for Person operations
+*/
+
+public class personTest {
+	String [] person_data;
+
+}
+
+public void setUp(){
+	Person person_obj = new Person();
+	Room room_obj =new Room();
+	person_data = new String[]{"SAMMY WANJALA","FELLOW", "Y"};
+}
+
+@Test
+public void test_that_a_new_fellow_has_been_added_successfully(){
+
+	assertEquals(person_obj(person_data),"FELLOW has been added successfully");
+
+}
+
+@Test
+public void test_reallocate_person(){
+
+	/*Assert that a user has been reallocated properly*/
+
+	}
+
