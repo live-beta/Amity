b12800e1f720b94e9f3f862dcca4ccd661f85822
live-beta <wanjala.sam.s@gmail.com>
2017-08-22 15:41:03 +0300

    room(refactor): add room functionality in a seperate class
    - ensure that the capacity field is added on room operations
    
    [Bulds #Task-01]

---
+++ com/java/Amity/RoomOps.java
@@ -0,0 +1,46 @@
+package RoomOps;
+
+/**
+* Class containing all the room functional operations.
+*
+*/
+
+
+public class RoomOps{
+	private List<Room> roomList = new ArrayList<Room>();
+
+	/* Methojd that adds a new room to the application*/
+
+	public String addRoom(){
+
+		String message = "New room has been added";
+
+		Room room = new Room();
+
+		room.setRoomName(roomName);
+		room.setCategory(roomCategory);
+
+		if (roomCategory =="OFFICE") {
+			int capacity = 6;
+
+			room.setCapacity(capacity);
+			
+		}else if(roomCategory=="LIVING"){
+			int capacity = 4;
+
+			room.setCapacity(capacity);
+
+		} else {
+			message = "This room category does not exist";
+			return message;
+		}
+
+		room.setRoomName(roomName);
+		room.setCategory(roomCategory);
+		room.setCapacity(roomCapacity);
+
+		roomList.add(room);
+
+		return message;
+	}
+}
