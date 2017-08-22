f2b4bc66d43e852d7628313c9ef72c9d45dc7587
live-beta <wanjala.sam.s@gmail.com>
2017-08-22 12:54:22 +0300

    refactor(assignment): change constructor assignment
    - ensure that data is assigned through construcors
    
    [Builds #Task-01]

--- Tests/personTests.java
+++ Tests/personTests.java
@@ -10,32 +10,31 @@ import junit.framework.*;
 public class personTest {
 	String [] person_data;
 
-
 public void setUp(){
 	Person person_obj = new Person();
 	Room room_obj =new Room();
 	person_data = new String[]{"SAMMY WANJALA","FELLOW", "Y"};
 }
-
+ /* Test that fellow has been adde successfully to the system*/
 @Test
 public void test_that_a_new_fellow_has_been_added_successfully(){
+
 
 	assertEquals(person_obj(person_data),"FELLOW has been added successfully");
 
 }
-
+/* Test that fellow has been reallocated successfully*/
 @Test
 public void test_reallocate_person(){
-
-	/*Assert that a user has been reallocated properly*/
 
 	assertEquals(person_obj(person_data)," Reallocation success")
 
 	}
-
+/* Test that a fellows data is typed correctly in the appliaction*/
 @Test
 public void test_that_person_data_is_of_correct_type(){
 
    assertTrue(person_data instanceof Array);
 }
+
 }
--- com/java/Amity/Person.java
+++ com/java/Amity/Person.java
@@ -3,18 +3,71 @@ package Person;
 /**
 * Class that contains functionality for handling people
 */
+
 public class Person extends Room {
 
- /* Constructor function*/
 
- public void Person(){
+ private String name,category;
+ private char wants_accomodation;
 
- 	String name, category,wants_accomodation;
- 	String room_name;
- 	String officeSpaceName;
+ /* Contructor function for the initialioskatin of data items */
+
+
+ public Person(String name, String category, char wants_accomodation = 'N'){
+
+ 	this.name = setName(name);
+ 	this.category = setCategory(category);
+ 	this.wants_accomodation = setAccomodation(wants_accomodation);
+ 	
  }
+
+ /* Sets up the users name*/
+
+ public void setName(String name){
+
+ 	this.name = name;
+
+ } 
+ /* Sets up the users category */
+
+ public void setCategory(String category){
+
+ 	this.category = category;
+ }
+/* sets up accomodation*/
+
+public void setAccomodation(String accomodation){
+
+	this.accomodation = accomodation;
+}
+
+ /* Settin up setters*/
+
+/*Gets the persons name*/
+ public String getName (){
+
+ 	return name;
+ }
+
+ /* Gets the persons category*/
+ public String getCategory(){
+
+ 	return category;
+
+ }
+ /*Gets the persons accomdation preference*/
+
+ public Char getAccomodation(){
+
+ 	return wants_accomodation;
+ }
+
+ /* Method to add a new person to the application*/
+
  public String add_person(){
  	String message;
+ 	this.name = getName();
+
 
  	return message;
  }
