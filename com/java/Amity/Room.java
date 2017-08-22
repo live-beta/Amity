1a619d6eaa2961fbd54a53165ab7b6727beccbb2
live-beta <wanjala.sam.s@gmail.com>
2017-08-22 15:22:34 +0300

    room(refactor_room): refactor room class to setters and getters
    - Ensure that the data items are encapsulated in the room class
    
    [Build #Task-01]

--- com/java/Amity/Room.java
+++ com/java/Amity/Room.java
@@ -1,62 +1,37 @@
+package Room;
+
 /**
 * Class that manages the data and functionality
 * of rooms
 */
 public class Room extends People {
 
-	/*Constructor class*/
-	public void room(String room_names){
+	private String roomName, category;
 
-		String [] roomNames= new String []{roomNames};
+	public Room (String roomName, String category){
 
-		List<String> roomList = new ArrayLists<String>();
-		String message;
-
-
+		this.roomName= roomName;
+		this.category = category;
 	}
-	public String add_room(){
-       
-       /* Assign the correct room type */
-     if (roomName.length == 0) {
-     	message = "There'e are no rooms to save";
-     	return message;
 
-     } else {
+	/* Set room name*/
+	public void setRoomName(String roomName){
 
- 		for (int i =0; i < roomNames.length; i++ ) {
-
- 			if (roomNames[i].room_type == "OFFICE"){
-
- 				room_capacity = 6;
-
- 				roomList.add("roomName":roomNames[i],"roomCapacity":room_capacity, "roomType":room_type, "occupants":occupants);
-
- 				} else if (room_type == "LIVING") {
- 					room_capacity = 4;
-
- 			} else {
-
- 				message = "The room type is not correct";
- 					return message;
- 			}
-
+		this.roomName = roomName;
 	}
-			return message;
-}
+	/* Set room category*/
 
-}	
-	public String assignRoom(){
-		/* this method is invoked from a person method */
-
-		// check data fom an already existing list 
-		// ensure that the method only fdoes on e thing in returning the required room 
-		// test that the room theat is returned is of the correct order. 
-
-
+	public void setRoomCategory(String roomCategory){
+		this.roomCategory = roomCategory;
 	}
-	
-	public String reallocateRoom(){
+	/* Get room name*/
+	public String getRoomName(){
+		return roomName;
+	}
+	/* Get room category*/
 
+	public String getRoomCategory(){
+		return roomCategory;
 	}
 
 }
